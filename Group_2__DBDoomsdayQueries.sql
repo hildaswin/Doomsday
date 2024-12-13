0a7e7181f5bffd7a0dbf322fa8e0702c3a2ce13a
AlexisB-SCTCC <ls5754vw@go.minnstate.edu>
2024-12-13 14:08:16 -0600

    Added view "view_GreenhouseNextWaterDate" and removed semicolons after a few "GO"s

--- Group_2__DBDoomsdayQueries.sql
+++ Group_2__DBDoomsdayQueries.sql
@@ -60,7 +60,7 @@ SELECT TOP 10
 	virusDangerRating AS DangerRating
 FROM Virus
 ORDER BY virusDangerRating DESC;
-GO;
+GO
 
 -- Top 10 best lodgings.
 -- Only includes lodgings from safe locations.
@@ -75,7 +75,7 @@ INNER JOIN Locations AS lc
 	ON lg.locationKey = lc.locationKey
 WHERE lc.locationSafe = 1
 ORDER BY lg.lodgingComfortRating DESC;
-GO;
+GO
 
 -- Resources Shared Between Factions
 -- identifies shared items among multiple factions.
@@ -89,7 +89,7 @@ INNER JOIN Items i ON inv.itemKey = i.itemKey
 GROUP BY inv.itemKey, i.itemName
 HAVING COUNT(DISTINCT inv.factionKey) > 1
 ORDER BY NumFactions DESC;
-GO;
+GO
 
 --Find Locations with Most Resources
 --locations with the most variety of items in their inventory.
@@ -104,7 +104,7 @@ INNER JOIN Inventory inv ON f.factionKey = inv.factionKey
 INNER JOIN Items i ON inv.itemKey = i.itemKey
 GROUP BY l.locationKey, l.locationName
 ORDER BY TotalItems DESC;
-GO;
+GO
 
 -- Retrieves all viruses without any recorded transmission methods.
 CREATE VIEW view_VirusWithoutTransmission
@@ -140,4 +140,18 @@ FROM Locations AS lc
 LEFT JOIN Lodging AS lg
 	ON lc.locationKey = lg.locationKey
 WHERE lg.locationKey IS NULL;
+GO
+
+-- Retrieves all spots in the Greenhouse, the plants growing in them, the date they were last watered,
+-- and calculates when next they should be watered.
+CREATE VIEW view_GreenhouseNextWaterDate -- AB
+AS
+	SELECT gh.spotKey AS Spot_Key,
+		pl.plantWaterFrequency AS Water_Frequency,
+		pl.plantName AS Plant_Name, 
+		gh.lastWatered AS Last_Watered,
+		FORMAT(DATEADD(DAY, pl.plantWaterFrequency, gh.lastWatered), 'd') AS Next_Watering
+	FROM Greenhouse AS gh
+	JOIN Plants AS pl ON pl.plantKey = gh.plantKey
+	ORDER BY DATEADD(DAY, pl.plantWaterFrequency, gh.lastWatered) OFFSET 0 ROWS;
 GO
