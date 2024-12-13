831e566b75c7777b3e084efe68567709fa06fc8f
hildaswin <94645708+hildaswin@users.noreply.github.com>
2024-12-13 13:04:03 -0600

    Update Group_2_DBDoomsdaySPROCS.sql

--- Group_2_DBDoomsdaySPROCS.sql
+++ Group_2_DBDoomsdaySPROCS.sql
@@ -1,68 +1,6 @@
 USE Doomsday
 GO;
 
-
-
-
--- ===== Queries
-
-
--- Count the number of viruses for each danger rating.
-CREATE VIEW view_VirusCountForEachDangerRating 
-AS
-SELECT virusDangerRating AS DangerRating,
-	COUNT(virusKey) AS NumOfViruses
-FROM Virus
-GROUP BY virusDangerRating
-GO
-
--- Count the number of viruses for each transmission method.
-CREATE VIEW view_VirusCountForEachTransmission
-AS
-SELECT t.transmissionMethodKey AS TransmissionID,
-	t.transmissionMethod AS TransmissionMethod,
-	COUNT(vt.virusKey) AS NumOfViruses
-FROM VirusTransmission AS t
-LEFT JOIN VirusTransmissionDetails AS vt
-	ON t.transmissionMethodKey = vt.transmissionMethodKey
-GROUP BY t.transmissionMethodKey,
-	t.transmissionMethod
-GO
-
--- Determine the water source rating of all location.
--- Best water source means the average value between safety and abundance rating is 10.
-CREATE VIEW view_WaterSourceRating
-AS
-SELECT w.waterName AS WaterSource,
-		w.locationKey AS LocationID,
-		l.locationName AS LocationName,
-		w.waterSafetyRating AS SafetyRating,
-		w.waterAbundanceRating AS AbundanceRating,
-		CAST(((w.waterSafetyRating + w.waterAbundanceRating) / 2.0) AS DECIMAL(3, 1)) AS SourceRating
-FROM water AS w
-INNER JOIN locations AS l
-	ON w.locationKey = l.locationKey
-GO
-
--- Lodgings from safe locations.
-CREATE VIEW view_SafeLodgings
-AS
-SELECT lg.locationKey AS LocationID,
-	lc.locationName AS LocationName,
-	lg.lodgingKey AS LodgingID,
-	lg.lodgingName AS LodgingName,
-	lg.lodgingComfortRating AS ComfortRating
-FROM Lodging AS lg
-INNER JOIN Locations AS lc
-	ON lg.locationKey = lc.locationKey
-WHERE lc.locationSafe = 1
-GO
-
-
-
--- ===== Stored Procedures
-
-
 -- Retrieves the transmission methods for the specified virus,
 -- passing the virus id or name.
 CREATE PROCEDURE sp_VirusTransmissionMethods
@@ -226,33 +164,5 @@ BEGIN
 		@locationName VARCHAR(50)', 
 		@locationKey, 
 		@locationName;
-END;
-GO
-
-
-
--- ===== Triggers
-
-
--- Triggers when a new virus or an old virus has been added/updated with a
--- danger rating of 10.
-CREATE TRIGGER trg_TriggerWhenNewDeadlyVirusDetected
-ON Virus
-AFTER INSERT, UPDATE
-AS
-BEGIN
-	DECLARE @virusKey VARCHAR(8);
-	DECLARE @virusName VARCHAR(50);
-	
-	SELECT @virusKey = i.virusKey FROM inserted AS i;
-	SELECT @virusName = i.virusName FROM inserted AS i;
-
-	IF EXISTS (SELECT 1 FROM inserted AS i WHERE i.virusDangerRating = 10)
-	BEGIN
-		DECLARE @message VARCHAR(200) = 'Virus with a danger rating of 10 has been detected. VirusID: ' +
-										@virusKey + ' - VirusName: ' + @virusName;
-
-		RAISERROR(@message, 16, 1);
-	END
 END;
 GO
