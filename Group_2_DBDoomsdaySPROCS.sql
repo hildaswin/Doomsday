2596dff1261c29ac29b3df628e1f1af108758063
AlexisB-SCTCC <ls5754vw@go.minnstate.edu>
2024-12-13 16:20:26 -0600

    Added two stored procedures

--- Group_2_DBDoomsdaySPROCS.sql
+++ Group_2_DBDoomsdaySPROCS.sql
@@ -166,3 +166,82 @@ BEGIN
 		@locationName;
 END;
 GO
+
+
+
+-- Updates the lastWatered column of a row in the Greenhouse
+CREATE PROCEDURE sp_WaterPlant -- AB
+@spotKey AS VARCHAR(8) = NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	IF @spotkey IS NOT NULL
+		AND EXISTS (SELECT * FROM Greenhouse WHERE spotKey = @spotKey)
+	BEGIN
+		UPDATE Greenhouse
+		SET lastWatered = GETDATE()
+		WHERE spotKey = @spotKey;
+		PRINT 'Plant Watered Successfully';
+	END
+	ELSE PRINT 'Unable to water plant. Please check the input spotKey.'
+END;
+GO
+
+
+-- Adds either a new weapon to the Items table, a new Ammo type to the Ammo table, or both.
+-- If both are added, may also connect them via the WeaponAmmo table
+CREATE PROCEDURE sp_AddWeaponAndOrAmmo -- AB
+@addWeapon AS BIT = 0,
+@weaponName AS VARCHAR(20),
+@weaponInfo AS TEXT = NULL,
+
+@addAmmo AS BIT = 0,
+@ammoName AS VARCHAR(20),
+
+@isAmmoForWeapon AS BIT = 0
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	-- Get the typeKey of the Weapon item type. This helps protect the procedure from future changes to the ItemType table.
+	DECLARE @itemTypeWeapon AS VARCHAR(8) = (SELECT TOP 1 typeKey FROM ItemType WHERE typeName = 'Weapon');
+
+	IF @addWeapon = 1
+	BEGIN
+		-- Establishes the itemKey of the new weapon
+		DECLARE @newestItemKey AS VARCHAR(8) = (SELECT TOP 1 itemKey FROM Items ORDER BY itemKey DESC); -- Get the newest added itemKey from the Items table
+		DECLARE @nextItemKeyNum AS INT = CAST(SUBSTRING(@newestItemKey, 3, LEN(@newestItemKey)) AS INT ) + 1; -- Get the number from the end of the newest itemKey
+		-- Concat 'IT', trailing zeroes, and the number of the new key to form the full key
+		DECLARE @weaponKey AS VARCHAR(8) = CONCAT('IT', REPLICATE('0', 4-LEN(@nextItemKeyNum)), @nextItemKeyNum);
+
+		INSERT INTO Items(itemKey, typeKey, itemName, itemInfo)
+		VALUES(@weaponKey, @itemTypeWeapon, @weaponName, @weaponInfo);
+
+		PRINT CONCAT('Weapon "', @weaponName, '" added successfully');
+	END
+
+	IF @addAmmo = 1
+	BEGIN
+		-- Establishes the key of the new ammo
+		DECLARE @newestAmmoKey AS VARCHAR(8) = (SELECT TOP 1 ammoKey FROM Ammo ORDER BY ammoKey DESC); -- Get the newest added ammoKey from the Ammo table
+		DECLARE @nextAmmoKeyNum AS INT = CAST(SUBSTRING(@newestAmmoKey, 3, LEN(@newestAmmoKey)) AS INT ) + 1; -- Get the number from the end of the newest ammoKey
+		-- Concat 'AM', trailing zeroes, and the number of the new key to form the full key
+		DECLARE @ammoKey AS VARCHAR(8) = CONCAT('AM', REPLICATE('0', 4-LEN(@nextAmmoKeyNum)), @nextAmmoKeyNum);
+
+		INSERT INTO Ammo(ammoKey, ammoName)
+		VALUES(@ammoKey, @ammoName);
+
+		PRINT CONCAT('Ammo type "', @ammoName, '" added successfully');
+	END
+
+	IF @isAmmoForWeapon = 1 AND @addAmmo = 1 AND @addWeapon = 1
+	BEGIN
+		INSERT INTO WeaponAmmo(itemKey, ammoKey)
+		VALUES (@weaponKey, @ammoKey);
+		PRINT ('Weapon and Ammo linked successfully.');
+	END
+END;
+GO
+
